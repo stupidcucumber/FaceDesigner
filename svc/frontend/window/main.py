cda166fe885537be31d9e368a8e6963d790e83f3
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2024-03-10 14:40:24 +0200

    Added toolbar

--- svc/frontend/window/main.py
+++ svc/frontend/window/main.py
@@ -1,9 +1,22 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QMainWindow, QWidget
+from PyQt6.QtWidgets import (
+    QMainWindow, 
+    QToolBar
+)
+from PyQt6.QtGui import QAction
 
 
 class MainWindow(QMainWindow):
     def __init__(self, title: str='FaceDesigner 🔥') -> None:
         super(MainWindow, self).__init__()
-
         self.setWindowTitle(title)
+        self._add_toolbar()
+
+    def _add_toolbar(self) -> None:
+        toolbar = QToolBar('main-toolbar')
+        open_button = QAction('Open', toolbar)
+        save_button = QAction('Save', toolbar)
+        toolbar.addActions(
+            [open_button, save_button]
+        )
+        self.addToolBar(toolbar)
